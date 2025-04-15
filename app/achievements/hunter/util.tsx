52a10ed1fcfda99d3b46d2de0189e9f2b8c57e2f
Tommi Linnamaa <tommi.linnanmaa@gmail.com>
2025-04-15 21:55:51 +0300

    header fixed

--- app/achievements/hunter/util.tsx
+++ app/achievements/hunter/util.tsx
@@ -1,4 +1,4 @@
-import { Card } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
 import { IAchievement } from "@/lib/types";
 import { JSX } from "react";
@@ -9,22 +9,27 @@ interface ICardDetails {
 }
 export const createCard = (cardDetails: ICardDetails): JSX.Element => {
   return (
-    <Card key={cardDetails.name} className="flex justify-center items-center">
-      <h3>{cardDetails.name}</h3>
+    <Card key={cardDetails.name} className="w-full">
 
-      <Table>
-        <TableBody>
-          {cardDetails.achievements.map((achievement, rowIndex) => {
-            return (
-              <TableRow key={`${cardDetails.name}-${rowIndex}`}>
-                <TableCell>{achievement.name}</TableCell>
-                <TableCell>{achievement.description}</TableCell>
-                <TableCell>{achievement.points}</TableCell>
-              </TableRow>
-            );
-          })}
-        </TableBody>
-      </Table>
+      <CardHeader>
+        <CardTitle className="text-center">{cardDetails.name}</CardTitle>
+      </CardHeader>
+      <CardContent>
+
+        <Table>
+          <TableBody>
+            {cardDetails.achievements.map((achievement, rowIndex) => {
+              return (
+                <TableRow key={`${cardDetails.name}-${rowIndex}`}>
+                  <TableCell>{achievement.name}</TableCell>
+                  <TableCell>{achievement.description}</TableCell>
+                  <TableCell>{achievement.points}</TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </CardContent>
     </Card>
   );
 };
